f662abd14d401bc0f5a5e738d87d3fe58d66f375
nikita <nikita119@protonmail.com>
2025-05-03 16:37:53 +0300

    custom component & styles

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import "./EpenseItem.css";
+
+function ExpenseItem() {
+    return (
+      <div className="expense-item">
+          <div>March 1st 2025</div>
+          <div className="expense-item__description">
+              <h2>Car Insurance</h2>
+              <div className="expense-item__price">$300</div>
+          </div>
+      </div>
+    );
+}
+
+export default ExpenseItem;
